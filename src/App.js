62aedc0a0d2d28ed48a6e5c5d512f344dda32742
Kuma Chan <peacethekuma@gmail.com>
2018-12-11 13:06:19 +0800

    Fix bugs

--- src/App.js
+++ src/App.js
@@ -1,49 +1,73 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import Page1 from './components/Page1';
 
-import AsyncComponent from './components/AsyncComponent';
+import Page1 from './Components/Page1';
+// Part 1 - No Code Splitting
+// import Page2 from './Components/Page2';
+// import Page3 from './Components/Page3';
+// Part 3 - Cleaner Code Splitting
+import AsyncComponent from './Components/AsyncComponent';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       route: 'page1',
-      component: null
+      // Part 2 - Code Splitting - manual
+      // component: null
     }
   }
-
   onRouteChange = (route) => {
-    // No Code Splitting
+    // Part 1 - No Code Splitting
     this.setState({ route: route });
-    // With Code Splitting
-    // if(route === 'page1'){
-    //   this.setState({route:route})
-    // }else if(route === 'page2'){
-    //   import('./components/Page2').then((Page2)=>{
-    //     this.setState({route:route , component:Page2.default})})
-    // }else if(route === 'page3'){
-    //   import('./components/Page3').then((Page3)=>{
-    //     this.setState({route:route , component:Page3.default})})
+    // Part 2 - Code Splitting - manual
+    // if (route === 'page1') {
+    //   this.setState({ route: route })
+    // } else if (route === 'page2') {
+    //   import('./Components/Page2')
+    //     .then((Page2) => {
+    //       this.setState({ route: route, component: Page2.default })
+    //     })
+    //     .catch(err => {
+    //     });
+    // } else {
+    //   import('./Components/Page3')
+    //     .then((Page3) => {
+    //       this.setState({ route: route, component: Page3.default })
+    //     })
+    //     .catch(err => {
+    //       console.log(err)
+    //     });
     // }
   }
-
   render() {
-    if (this.state.route === 'page1') {
-      return <Page1 onRouteChange={ this.onRouteChange } />
-    } else if (this.state.route === 'page2') {
-      const AsyncPage2 = AsyncComponent(() => import('./components/Page2'));
-      return <AsyncPage2 onRouteChange={ this.onRouteChange } />
-    } else if (this.state.route === 'page3') {
-      const AsyncPage3 = AsyncComponent(() => import('./components/Page3'));
-      return <AsyncPage3 onRouteChange={ this.onRouteChange } />
-    }
-    // if(this.state.route === 'page1'){
-    //   return <Page1 onRouteChange={this.onRouteChange}/>
-    // }else{
-    //   return <this.state.component onRouteChange={this.onRouteChange}/>
+    // Part 1 - No code splitting
+    // if (this.state.route === 'page1') {
+    //   return <Page1 onRouteChange={this.onRouteChange} />
+    // } else if (this.state.route === 'page2') {
+    //   return <Page2 onRouteChange={this.onRouteChange} />
+    // } else {
+    //   return <Page3 onRouteChange={this.onRouteChange} />
     // }
+
+    // Part 2 - No Code Splitting - manual
+    // if (this.state.route === 'page1') {
+    //   return <Page1 onRouteChange={this.onRouteChange} />
+    // } else {
+    //   return <this.state.component onRouteChange={this.onRouteChange} />
+    // }
+
+    // Part 3 - Cleaner Code Splitting
+    if (this.state.route === 'page1') {
+      return <Page1 onRouteChange={this.onRouteChange} />
+    } else if (this.state.route === 'page2') {
+      const AsyncPage2 = AsyncComponent(() => import("./Components/Page2"));
+      return <AsyncPage2 onRouteChange={this.onRouteChange} />
+    } else {
+      const AsyncPage3 = AsyncComponent(() => import("./Components/Page3"));
+      return <AsyncPage3 onRouteChange={this.onRouteChange} />
+    }
+
   }
 }
 
--- src/components/AsyncComponent.js
+++ src/components/AsyncComponent.js
@@ -1,25 +1,25 @@
-import React from 'react';
-
+import React, { Component } from "react";
 
 export default function asyncComponent(importComponent) {
-
-  class AsyncComponent extends React.Component {
+  class AsyncComponent extends Component {
     constructor(props) {
       super(props);
       this.state = {
         component: null
-      }
+      };
     }
 
     async componentDidMount() {
-      const component = await importComponent();
+      const { default: component } = await importComponent();
+
       this.setState({
-        component: component.defualt
-      })
+        component: component
+      });
     }
 
     render() {
       const Component = this.state.component;
+
       return Component ? <Component { ...this.props } /> : null;
     }
   }
